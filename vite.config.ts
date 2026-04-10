7f43dcdd82022e8b1b6f84a13490e0b21bc3d946
Eloim <eloimm@gmail.com>
2026-04-10 11:53:11 -0300

    fix: adicionando permissões de id-token

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/lp_cursoaiv/",
+  base: './',
   server: {
     host: "::",
     port: 8080,
